d2dc50c1b19272d0f58d4dbf6f6508486a1d852a
aravindcad01 <aravind2k23cad01@gmail.com>
2024-02-20 16:41:27 +0530

    added c#

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// class Aravindk
+// {
+//     static void Main(string [] args)
+//     {
+//     Console.WriteLine("Added value");
+//     Arav k=new Arav();
+//     k.cal();
+
+//     }
+// }
+// public class Arav
+// {
+    
+//     public void cal()
+//     {
+//         int a=20;
+//         int b=30;
+//         int c;
+//         c= a + b;
+//         Console.WriteLine(c);
+// }
+// }
+
+class Aravind 
+{
+    static void Main(string [] args)
+    {
+a1 s=new a1();
+s.add();
+s.sub();
+s.mul();
+
+
+    }
+}
+
+
+public class a1
+{
+    public void add()
+    {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        Console.WriteLine(c);
+    }
+
+
+    public void sub()
+    {
+        int a=23;
+        int b=45;
+        int c=a-b;
+        Console.WriteLine(c);
+    }
+
+    public void mul()
+    {
+        int a=50;
+        int b=100;
+        int c=a*b;
+        Console.WriteLine(c);
+    }
+}
