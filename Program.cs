3aaef26c6cc06ad66935b1b9b41d2f008ea24a19
aravindcad01 <aravind2k23cad01@gmail.com>
2024-02-21 18:03:34 +0530

    added

--- Program.cs
+++ Program.cs
@@ -21,44 +21,77 @@
 // }
 // }
 
-class Aravind 
-{
-    static void Main(string [] args)
-    {
-a1 s=new a1();
-s.add();
-s.sub();
-s.mul();
+// class Aravind 
+// {
+//     static void Main(string [] args)
+//     {
+// a1 s=new a1();
+// s.add();
+// s.sub();
+// s.mul();
+// s.div();
+
+//     }
+// }
 
 
-    }
+// public class a1
+// {
+//     public void add()
+//     {
+//         int a=10;
+//         int b=20;
+//         int c=a+b;
+//         Console.WriteLine(c);
+//     }
+
+
+//     public void sub()
+//     {
+//         int a=23;
+//         int b=45;
+//         int c=a-b;
+//         Console.WriteLine(c);
+//     }
+
+//     public void mul()
+//     {
+//         int a=50;
+//         int b=100;
+//         int c=a*b;
+//         Console.WriteLine(c);
+//     }
+
+
+    
+//         public void div()
+//         {
+//             int a=20;
+//             int b=10;
+//             int c=a/b;
+//             Console.WriteLine(c);
+//         }
+
+//     }
+
+
+single inheritance
+
+public class bca{
+    public string arav = "presnt today";
+
 }
 
+public class mca:bca{
+    public string harsh ="present";
+}
 
-public class a1
+class miet
 {
-    public void add()
-    {
-        int a=10;
-        int b=20;
-        int c=a+b;
-        Console.WriteLine(c);
-    }
+    public static void main(string [] args)(
+    mca i=new mca();
+    Console.WriteLine("i.bca");
+    Console.WriteLine("i.mca");
+    )
 
-
-    public void sub()
-    {
-        int a=23;
-        int b=45;
-        int c=a-b;
-        Console.WriteLine(c);
-    }
-
-    public void mul()
-    {
-        int a=50;
-        int b=100;
-        int c=a*b;
-        Console.WriteLine(c);
-    }
 }
